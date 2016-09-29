f74cc00c557a32891318cd7859ce1133297c5245
Manuel Guillen <manuelg@mit.edu>
2016-09-29 00:58:30 -0400

    Finalized canvas initial setup
    Also added missing <script> tag for objects.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,26 @@
-// FUNCTIONALS (provided in 6.170 lecture)
-// =======================================
 from_to = function (from, to, f) {
-  			if (from > to) return;
-			f(from); from_to(from+1, to, f);
-		  }
-
-each = function (a, f) {
-  			from_to(0, a.length-1, function (i) {f(a[i]);});
-	   }
-
-map = function (a, f) {
-  		var result = [];
-  		each (a, function (e) {
-    		result.push(f(e));
-    	});
-  		return result;
-  	  }
-
-filter = function (a, f) {
-  			var result = [];
-  			each (a, function (e) {
-    			if (f(e)) result.push(e);
-    		});
-  			return result;
-  		 }
-
-reduce = function (a, f, base) {
-  			var result = base;
-  			each (a, function (e) {
-    			result = f(e, result);
-    		});
-  			return result;
-  		 }
-
-// ======================================
-
-CELL_SIZE = 10
+  			     if (from > to) return;
+			       f(from); from_to(from+1, to, f);
+		      }
+// =================================================
 
 $(document).ready(function(e) {
   
+  // ================ CANVAS SETUP =================
   var canvas = $("#canvas")[0];
   var context = canvas.getContext("2d");
 
-  var boardState = []
+  const CELL_SIZE = 10;
+  var width = canvas.width/CELL_SIZE;
+  var height = canvas.height/CELL_SIZE;
 
   context.strokeStyle = "#DDDDDD";
 
-  from_to(0, canvas.height/CELL_SIZE, function(y) {
-    var row = []
-    from_to(0, canvas.width/CELL_SIZE, function(x) {
-      context.strokeRect(CELL_SIZE*x,CELL_SIZE*y,CELL_SIZE,CELL_SIZE);
-      row.push(false)
+  from_to(0, height-1, function(y) {
+    from_to(0, width-1, function(x) {
+      context.strokeRect(CELL_SIZE*x, CELL_SIZE*y, CELL_SIZE, CELL_SIZE);
     });
-    boardState.push(row)
   });
+  // ===============================================
 
 });
